5cfaab7aae2c4c799cb77325a3db49ee1a42c020
luxembourq <romanpypa@gmail.com>
2022-06-17 17:11:26 +0000

    html

--- sd.py
+++
@@ -1,9 +0,0 @@
-Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a=2
->>> for i in range(1000):
-	c=a+a/2
-	a=a/2
-
-	
->>> print(c)
